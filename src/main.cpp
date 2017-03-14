2def452975ab06a3de958e7dba830a1491369c6d
YeonhwaCha <yeonhwacha@Yeonhwas-MacBook-Pro.local>
2017-03-14 22:52:28 +0900

    create project and test commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,18 @@
 #include <string.h>
 #include "httpServer.hpp"
 
-
 int main() {
+  struct sockaddr_in clientaddr;
   char PORT[6];
   strcpy(PORT, "8090");
-  httpServer::createHttpServer(PORT);
+
+  if (httpServer::createHttpServer(PORT) != -1) {
+    printf("create server success!!\n");
+  }
+
+  while(1) {
+    
+  }
+
   return 0;
 }
