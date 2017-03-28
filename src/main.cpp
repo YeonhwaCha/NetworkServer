2c83206f88a70f277e7286e90bdcf32d160bb81f
YeonhwaCha <yeonhwacha@Yeonhwas-MacBook-Pro.local>
2017-03-28 23:50:23 +0900

    [modified] 1. add accept() function

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/socket.h> //relate socklen_t
 #include <netinet/in.h> //relate sockaddr_in
@@ -10,7 +11,9 @@ int main() {
   int clients[Server.CONNMAX];
   struct sockaddr_in clientaddr;
   socklen_t addrlen;
- 
+  char buffer[256]; //I wonder if fixed array size is good??
+  int numOfReadWrite;
+
   strcpy(PORT, "8090");
 
   if (Server.createHttpServer(PORT) != -1) {
@@ -19,7 +22,39 @@ int main() {
 
   while(1) {
     addrlen = sizeof(clientaddr);
-    
+    //[step3] accept()
+    clients[slot] = accept(Server.listenfd, (struct sockaddr *) &clientaddr, &addrlen);
+
+    if (clients[slot] < 0) {
+      printf("There is no client\n");
+    } else {
+      bzero(buffer, 256);
+      //read()
+      numOfReadWrite = read(clients[slot], buffer, 255);
+
+      if (numOfReadWrite < 0) {
+	printf("read error : reading from socket\n");
+      } else {
+	printf("[read] :: %s\n", buffer);
+      }
+      //read() end
+
+      //write()
+      numOfReadWrite = write(clients[slot], "I got your message", 18);
+
+      if (numOfReadWrite < 0) {
+	printf("write error : writing from socket\n");
+      } else {
+	close(clients[slot]);
+	close(Server.listenfd);
+      }
+      //write() end
+      
+    }
+
+    while (clients[slot] != -1) {
+      slot = (slot+1)%Server.CONNMAX;
+    }
   }
 
   return 0;
